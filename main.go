aa40b728336bfd2790e5007281661241100528d9
Martin Sahlen <martin8900@gmail.com>
2017-06-02 22:22:46 -0400

    Make it more elegant

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
+	"bufio"
+	"io"
 	"os"
 )
 
 func main() {
-	stdin, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
-	}
-	splitted := bytes.Split(stdin, []byte("\n"))
-	if len(splitted) > 1 && string(splitted[len(splitted)-1]) == "" {
-		splitted = splitted[:len(splitted)-2]
-	}
+	stdin := bufio.NewReader(os.Stdin)
 	os.Stdout.Write([]byte("["))
-	for index, line := range splitted {
-		if index == len(splitted)-1 {
-			os.Stdout.Write(line)
-			os.Stdout.Write([]byte("]"))
-		} else {
-			os.Stdout.Write(line)
+	for {
+		bytes, err := stdin.ReadBytes(byte('\n'))
+		if err == io.EOF {
+			break
+		}
+		if string(bytes) == "" {
+			continue
+		}
+		os.Stdout.Write(bytes)
+		b, _ := stdin.Peek(1)
+		if len(b) > 0 {
 			os.Stdout.Write([]byte(","))
 		}
 	}
+
+	os.Stdout.Write([]byte("]"))
 	os.Stdout.Write([]byte("\n"))
 }
